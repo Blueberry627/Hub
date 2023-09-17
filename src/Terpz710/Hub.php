8a263b023a93285a59c86cff93efdbfd58dfe1f0
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-16 20:21:56 -0700

    Update Hub.php

--- src/Terpz710/Hub.php
+++ src/Terpz710/Hub.php
@@ -8,7 +8,7 @@ use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-use pocketmine\player\Player;
+use pocketmine\world\WorldManager;
 use Terpz710\command\HubCommand;
 use Terpz710\command\SetHubCommand;
 
@@ -16,12 +16,18 @@ class Hub extends PluginBase implements Listener {
 
     public function onEnable() : void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getCommandMap()->register("hub", new HubCommand($this));
+        $this->getServer()->getCommandMap()->register("hub", new HubCommand());
+        $this->getServer()->getCommandMap()->register("sethub", new SetHubCommand($this->getWorldManager()));
+        $this->getWorldManager()->getDefaultWorld();
     }
 
     public function onPlayerDeath(PlayerDeathEvent $event) {
         $player = $event->getPlayer();
         $spawnLocation = $player->getWorld()->getSpawnLocation();
         $player->teleport($spawnLocation);
+    }
+
+    public function getWorldManager(): WorldManager {
+        return $this->getServer()->getWorldManager();
     }
 }
