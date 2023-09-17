3fb35bdd1dd8fc76a0d14d43b8c205bc8b5aedac
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-16 19:55:12 -0700

    Add files via upload

---
+++ src/Terpz710/command/SetHubCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Terpz710\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use Terpz710\Hub as Main;
+use pocketmine\world\WorldManager;
+
+class SetHubCommand extends Command {
+
+    private $plugin;
+    private WorldManager $worldManager;
+
+    public function __construct(Main $plugin, WorldManager $worldManager) {
+        $this->plugin = $plugin;
+        $this->worldManager = $worldManager;
+        parent::__construct(
+            "sethub",
+            "Set the hub",
+            "/sethub <x> <y> <z>",
+            ["setlobby", "setspawn"]
+        );
+        $this->setPermission("sethub.command");
+    }
+
+    public function getPlugin() : Plugin {
+        return $this->plugin;
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args) {
+        if (!$this->testPermission($sender)) {
+            return;
+        }
+
+        if ($sender instanceof Player) {
+            if (isset($args[0]) && isset($args[1]) && isset($args[2])) {
+                $x = $args[0];
+                $y = $args[1];
+                $z = $args[2];
+
+                $pos = new Vector3($x, $y, $z);
+                $pos->round();
+
+                $sender->getLevel()->setSpawnLocation($pos);
+                $sender->sendMessage(TextFormat::GREEN . "Hub location set to ($x, $y, $z)");
+            } else {
+                $sender->sendMessage(TextFormat::RED . "Please enter all three coordinates");
+            }
+        } else {
+            $sender->sendMessage(TextFormat::RED . "This command can only be used by players");
+        }
+    }
+}
